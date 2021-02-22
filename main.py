86bc663c45ed65763677ad8b4d6ab4e9201ffb52
Ryan Haver <ryanhaver168@gmail.com>
2021-02-22 11:52:37 -0500

    More GUI work, switching windows for login basics

--- main.py
+++ main.py
@@ -1,22 +1,63 @@
 import tkinter as tk
-import ptui
 
-root = tk.Tk()
-root.title("Password Manager")
-#root.geometry("300x500")
+class SampleApp(tk.Tk):
 
-u = tk.Label(root, text="Username:")
-u.grid(row=0, column=0, pady=5, padx = 25)
-p = tk.Label(root, text="Password:")
-p.grid(row=1, column=0, pady=5, padx = 25)
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.title("Password Manager")
+        self.geometry("500x500")
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-u_entry = tk.Entry(root)
-u_entry.grid(row=0, column=1)
+        self.frames = {}
+        for F in (LoginPage, Manager):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
 
-p_entry = tk.Entry(root)
-p_entry.grid(row=1, column=1)
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0)
 
-login = tk.Button(root, text="Login", padx = 15, command=ptui.generator)
-login.grid(row=0, column=2)
+        self.show_frame("LoginPage")
 
-root.mainloop()
+    def show_frame(self, page_name):
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+
+class LoginPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is the start page")
+        label.pack(side="top", fill="x", pady=10)
+
+        button1 = tk.Button(self, text="Go to Page One",
+                            command=lambda: controller.show_frame("Manager"))
+        button1.pack()
+        #button2.pack()
+
+
+class Manager(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is page 1")
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("LoginPage"))
+        button.pack()
+
+
+if __name__ == "__main__":
+    app = SampleApp()
+    app.mainloop()
