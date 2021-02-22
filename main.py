f6802da44c88ae8b9fc6b0c8dac9b616da1d643a
Ryan Haver <ryanhaver168@gmail.com>
2021-02-21 23:11:28 -0500

    Started GUI implementation

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 import tkinter as tk
-
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
-
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
-
-    def say_hi(self):
-        print("hi there, everyone!")
+import ptui
 
 root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+root.title("Password Manager")
+#root.geometry("300x500")
+
+u = tk.Label(root, text="Username:")
+u.grid(row=0, column=0, pady=5, padx = 25)
+p = tk.Label(root, text="Password:")
+p.grid(row=1, column=0, pady=5, padx = 25)
+
+u_entry = tk.Entry(root)
+u_entry.grid(row=0, column=1)
+
+p_entry = tk.Entry(root)
+p_entry.grid(row=1, column=1)
+
+login = tk.Button(root, text="Login", padx = 15, command=ptui.generator)
+login.grid(row=0, column=2)
+
+root.mainloop()
