1d3f7d3477eb28553ffcc739759f313452651a68
Ryan Haver <ryanhaver168@gmail.com>
2021-02-27 15:31:07 -0500

    More GUI work, added storage and partial retrieval functionality

--- gui_helper.py
+++ gui_helper.py
@@ -29,33 +29,15 @@ def store(website, usr, pswd):
     connection.close()
 
 
-def retrieve():
-    print("Select Option:")
-    print("1. Enter specific website for retrieval")
-    print("2. Browse all saved passwords")
-    print("3. Quit")
-    q = input()
-    
-    while (True):
-        if (q == '1' or q == '2' or q == '3'):
-            break
-        
-        else:
-            print("Error: Incorrect input. Please try again")
-            q = input()
+def retrieve_specific(name):
+    connection = sqlite3.connect("data.db")
+    c = connection.cursor()
+    c.execute('SELECT * FROM entry WHERE website=?', (name,))
+    value = c.fetchone()
+    connection.close()
+    return value
 
-    if (q == '1'):
-        w = input("Please enter the website name:")
-        connection = sqlite3.connect("data.db")
-        c = connection.cursor()
-        c.execute('SELECT * FROM entry WHERE website=?', (w,))
-        value = c.fetchone()
-        print("Username: " + value[1])
-        print("Password: " + value[2])
-        connection.close()
-        menu()
-
-
+def retrieve_all():
     if (q == '2'):
         connection = sqlite3.connect("data.db")
         c = connection.cursor()
@@ -65,73 +47,5 @@ def retrieve():
         for i in result:
             print(i[0], end =" ")
         print("\n")
-        menu()
 
-    if (q == '3'):
-        print("Have a good day!")
-        quit()
-
-def menu():
-    print("What would you like to do?")
-    print("1. Generate a password")
-    print ("2. Store a password")
-    print ("3. Retrieve a password")
-    print("4. Quit")
-
-    user =  input();
-
-    while (True):
-        if (user == '1' or user == '2' or user == '3' or user == '4'):
-            break
-        
-        else:
-            print("Error: Incorrect input. Please try again")
-            user = input()
-
-    if (user == '1'):
-        generator()
-    if (user == '2'):
-        web = input("Enter Website:")
-        u = input("Enter Username:")
-        p = input("Enter Password:")
-        print("Your password was stored")
-        store(web, u, p)
-    if (user == '3'):
-        retrieve()
-    if (user == '4'):
-        print("Have a good day!")
-        quit()
-
-def main():
-    print("Welcome to my password manager!\nWhat would you like to do?")
-    print("1. Generate a password")
-    print ("2. Store a password")
-    print ("3. Retrieve a password")
-    print("4. Quit")
-
-    user =  input();
-
-    while (True):
-        if (user == '1' or user == '2' or user == '3' or user == '4'):
-            break
-        
-        else:
-            print("Error: Incorrect input. Please try again")
-            user = input()
-
-    if (user == '1'):
-        generator()
-    if (user == '2'):
-        web = input("Enter Website:")
-        u = input("Enter Username:")
-        p = input("Enter Password:")
-        print("Your password was stored")
-        store(web, u, p)
-    if (user == '3'):
-        retrieve()
-    if (user == '4'):
-        print("Have a good day!")
-        quit()
-
-if (__name__ == "__main__"):
-    main()
+    
--- main.py
+++ main.py
@@ -14,6 +14,10 @@ def generator():
         u = StringVar()
         w = StringVar()
         
+        l = Label(text="Please enter information to \n be associated with that password")
+        l.pack(pady=3)
+        g_wig.append(l)
+
         user = Label(text="Username:")
         user.pack()
         g_wig.append(user)
@@ -27,7 +31,7 @@ def generator():
         g_wig.append(we)
 
 
-        web_e = Entry(textvariable = w, show="*")    
+        web_e = Entry(textvariable = w)    
         web_e.pack()
         g_wig.append(web_e)
 
@@ -35,6 +39,11 @@ def generator():
         submit = Button(text="Submit", command=lambda: gui_helper.store(w.get(),u.get(),p))
         submit.pack()
         g_wig.append(submit)
+
+
+        back = Button(text="Back", command=lambda:  clear(manager()))
+        back.pack(side="bottom")
+        g_wig.append(back)
 
     def gen(length):
         try: 
@@ -77,6 +86,111 @@ def generator():
     e.pack(side="bottom")
     g_wig.append(e)
 
+def store():
+    s_wig = []
+    def clear(func):
+        for i in s_wig:
+            i.pack_forget()
+        func
+    
+    u = StringVar()
+    ps = StringVar()
+    w = StringVar()
+
+    user = Label(text="Username:")
+    user.pack()
+    s_wig.append(user)
+
+    user_e = Entry(textvariable = u)
+    user_e.pack()
+    s_wig.append(user_e)
+
+    p = Label(text="Password:")
+    p.pack()
+    s_wig.append(p)
+    
+
+    pe = Entry(textvariable = ps)
+    pe.pack()
+    s_wig.append(pe)
+
+    we = Label(text="Website:")
+    we.pack()
+    s_wig.append(we)
+
+
+    web_e = Entry(textvariable = w)    
+    web_e.pack()
+    s_wig.append(web_e)
+
+    def submit():
+        gui_helper.store(w.get(),u.get(),ps.get())
+        clear(None)
+        success = Label (text="Password was saved")
+        success.pack()
+        s_wig.append(success)
+        back = Button(text="Back", command=lambda: clear(manager()))
+        back.pack()
+        s_wig.append(back)
+
+    
+    sub  = Button(text="Submit",  command=submit)
+    sub.pack(pady = 10)
+    s_wig.append(sub)
+
+
+def retrieve():
+    r_wig = []
+    def clear(func):
+        for i in r_wig:
+            i.pack_forget()
+        func
+
+    def get(name):
+        val = gui_helper.retrieve_specific(name)
+        u = Label(text="Username: " + val[1])
+        u.pack()
+        r_wig.append(u)
+
+        p = Label(text="Password: " + val[2])
+        p.pack()
+        r_wig.append(p)
+        
+        back = Button(text="Back", command=lambda: clear(manager()))
+        back.pack(side="bottom")
+        r_wig.append(back)
+
+    def specific():
+        clear(None)
+        w = Label(text="Website:")
+        w.pack()
+        r_wig.append(w)
+        
+        wv = StringVar()
+
+        we = Entry(textvariable=w)
+        we.pack()
+        r_wig.append(we)
+
+        sub  = Button(text="Submit", command=lambda: get(we.get()))
+        sub.pack()
+        r_wig.append(sub)
+
+
+
+    specific = Button(text="Enter specific website for retrieval", command=specific)
+    specific.pack()
+    r_wig.append(specific)
+
+
+    a = Button(text="Browse all saved passwords")
+    a.pack()
+    r_wig.append(a)
+
+
+    back = Button(text="Back", command=lambda: clear(manager()))
+    back.pack(side="bottom")
+    r_wig.append(back)
 
 def manager():
     m_widgets = []
@@ -92,10 +206,10 @@ def manager():
     g = Button(text="Generate a password", command=lambda: clear(generator()))
     g.pack()
     m_widgets.append(g)
-    s = Button(text="Store a password")
+    s = Button(text="Store a password", command=lambda: clear(store()))
     s.pack()
     m_widgets.append(s)
-    r = Button(text="Retrieve a password")
+    r = Button(text="Retrieve a password", command=lambda: clear(retrieve()))
     r.pack()       
     m_widgets.append(r)
 
